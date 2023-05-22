e5f44abcc3e7fecbd13061d575ed362147f6dc8c
Clo91eaf <Clo91eaf@qq.com>
2023-05-22 12:18:00 +0800

    feat<main>: parse section headers.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use core::prelude::rust_2015;
+
 use clap::Parser;
 use elf::abi::EI_VERSION;
 use elf::endian::AnyEndian;
-use elf::ElfBytes;
+use elf::section::SectionHeader;
+use elf::segment::ProgramHeader;
 use elf::to_str;
+use elf::ElfBytes;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -16,27 +20,53 @@ struct Args {
     file: std::path::PathBuf,
 }
 
+#[rustfmt::skip]
 fn parse_elf_header(ehdr: elf::file::FileHeader<AnyEndian>, ident: &[u8]) {
     println!("ELF Header:");
-    println!("  Magic: {:0x?}", ident);
-    println!("  Class: {:?}", ehdr.class);
-    println!("  Data: {:?}", ehdr.endianness);
-    println!("  Version: {:?}", ident[EI_VERSION]);
-    println!("  OS/ABI: {}", to_str::e_osabi_to_string(ehdr.osabi));
-    println!("  ABI Version: {:?}", ehdr.abiversion);
-    println!("  Type: {}", to_str::e_type_to_human_str(ehdr.e_type).unwrap());
-    println!("  Machine: {}", to_str::e_machine_to_human_str(ehdr.e_machine).unwrap());
-    println!("  Version: 0x{:x}", ehdr.version);
-    println!("  Entry point address: 0x{:x}", ehdr.e_entry);
-    println!("  Start of program headers: {:?}", ehdr.e_phoff);
-    println!("  Start of section headers: {:?}", ehdr.e_shoff);
-    println!("  Flags: 0x{:x}", ehdr.e_flags);
-    println!("  Size of this header: {:?}", ehdr.e_ehsize);
-    println!("  Size of program headers: {:?}", ehdr.e_phentsize);
-    println!("  Number of program headers: {:?}", ehdr.e_phnum);
-    println!("  Size of section headers: {:?}", ehdr.e_shentsize);
-    println!("  Number of section headers: {:?}", ehdr.e_shnum);
+    println!("  Magic:    {:02x?}", ident);
+    println!("  Class:                      {:?}", ehdr.class);
+    println!("  Data:                       {:?}", ehdr.endianness);
+    println!("  Version:                    {:?} (current)", ident[EI_VERSION]);
+    println!("  OS/ABI:                     {}", to_str::e_osabi_to_string(ehdr.osabi));
+    println!("  ABI Version:                {:?}", ehdr.abiversion);
+    println!("  Type:                       {}", to_str::e_type_to_human_str(ehdr.e_type).unwrap());
+    println!("  Machine:                    {}", to_str::e_machine_to_human_str(ehdr.e_machine).unwrap());
+    println!("  Version:                    0x{:x}", ehdr.version);
+    println!("  Entry point address:        0x{:x}", ehdr.e_entry);
+    println!("  Start of program headers:   {:?} (bytes into file)", ehdr.e_phoff);
+    println!("  Start of section headers:   {:?} (bytes into file)", ehdr.e_shoff);
+    println!("  Flags:                      0x{:x}", ehdr.e_flags);
+    println!("  Size of this header:        {:?} (bytes into file)", ehdr.e_ehsize);
+    println!("  Size of program headers:    {:?} (bytes into file)", ehdr.e_phentsize);
+    println!("  Number of program headers:  {:?}", ehdr.e_phnum);
+    println!("  Size of section headers:    {:?} (bytes into file)", ehdr.e_shentsize);
+    println!("  Number of section headers:  {:?}", ehdr.e_shnum);
     println!("  Section header string table index: {:?}", ehdr.e_shstrndx);
+}
+
+fn parse_section_headers(shdrs: Vec<SectionHeader>) {
+    println!("Section Headers:");
+    println!("  [Nr] Name              Type              Address            Offset");
+    println!("       Size              EntSize           Flags  Link  Info  Align");
+    for (i, shdr) in shdrs.iter().enumerate() {
+        println!(
+            "  [{:>2}] {:<17} {:<15}   {:016x}   {:08x}",
+            i,
+            shdr.sh_name,
+            to_str::sh_type_to_string(shdr.sh_type),
+            shdr.sh_addr,
+            shdr.sh_offset
+        );
+        println!(
+            "       {:016x}  {:016x}  {:<6} {:<5} {:<5} {:<5}",
+            shdr.sh_size,
+            shdr.sh_entsize,
+            shdr.sh_flags,
+            shdr.sh_link,
+            shdr.sh_info,
+            shdr.sh_addralign,
+        );
+    }
 }
 
 fn main() {
@@ -45,6 +75,8 @@ fn main() {
     let slice = file_data.as_slice();
     let file = ElfBytes::<AnyEndian>::minimal_parse(slice).unwrap();
     let ident = slice.get(0..16).unwrap();
+    let shdr: Vec<SectionHeader> = file.section_headers().unwrap().iter().collect();
+    let phdr: Vec<ProgramHeader> = file.segments().unwrap().iter().collect();
     parse_elf_header(file.ehdr, ident);
-    println!("Common sections: {:?}", file.ehdr);
+    parse_section_headers(shdr);
 }
